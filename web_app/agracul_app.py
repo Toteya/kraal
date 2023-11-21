a55dd73cd1e885f52214563b694194a3c7df61bb
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-21 16:17:41 +0200

    added static pages: signup, login; updated layout

---
+++ web_app/agracul_app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Runs the web application and renders the different views
+"""
+from flask import Flask, render_template, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    """
+    Routes to the welcome page
+    """
+    return render_template('home.html')
+
+@app.route('/home', strict_slashes=False)
+def home():
+    """
+    Routes to the home page
+    """
+    return render_template('main.html')
+
+@app.route('/login', strict_slashes=False)
+def login():
+    """
+    Routes to the login page
+    """
+    return render_template('login.html')
+
+@app.route('/signup', strict_slashes=False)
+def signup():
+    """
+    Routes to the signup page
+    """
+    return render_template('signup.html')
+
+
+if __name__ == '__main__':
+    host = os.getenv('WEB_APP_HOST', '0.0.0.0')
+    port = os.getenv('WEB_APP_PORT', 5000)
+    app.run(host=host, port=port)
