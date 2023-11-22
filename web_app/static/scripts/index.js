ac7c1aefb0cd3167a2044fec41bf27c070ffb79c
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-23 00:15:01 +0200

    updated layout, sign in formhandler linked to js

---
+++ web_app/static/scripts/index.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $( "#signupForm" ).on( "submit", function( event ) {
+        // alert( "Handler for `submit` called." );
+        // event.preventDefault();
+    });
+});
