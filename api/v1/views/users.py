3d09ce4dafbca79d95bc07257c30047c25118ffe
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-23 09:08:36 +0200

    users api: create a new user with POST request

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,19 +3,43 @@
 Contains users API views
 """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import abort, jsonify, request
 from models import storage
 from models.user import User
 
-@app_views.route('/users', strict_slashes=False)
+@app_views.route('/users',
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def users():
     """
     Return all users
     """
-    users = storage.all(User).values()
-    users_dicts = []
-    for user in users:
-        users_dicts.append(user.to_dict())
-    return jsonify(users_dicts)
+    if request.method == 'GET':
+        users = storage.all(User).values()
+        users_dicts = []
+        for user in users:
+            users_dicts.append(user.to_dict())
+        return jsonify(users_dicts)
+    if request.method == 'POST':
+        data = request.get_json(silent=True)
+        if not data:
+            abort(400, 'Not a valid JSON')
+        first_name = data.get('first_name')
+        if first_name is None:
+            abort(400, 'Missing: first_name')
+        last_name = data.get('last_name')
+        if last_name is None:
+            abort(400, 'Missing: last_name')
+        email = data.get('email')
+        if email is None:
+            abort(400, 'Missing: email')
+        password = data.get('password')
+        if password is None:
+            abort(400, 'Missing: password')
+        obj = User(**data)
+        obj.save()
+        return jsonify(obj.to_dict()), 201
+    
 
-# @app_views.route('users/')
+# @app_views.route('/users')
+# def user
