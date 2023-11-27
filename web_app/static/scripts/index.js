bce046e188e3bac05d41fb2f0809f934ae619e55
Toteya <111172685+Toteya@users.noreply.github.com>
2023-11-27 11:29:41 +0200

    implement sign up

--- web_app/static/scripts/index.js
+++ web_app/static/scripts/index.js
@@ -1,7 +1,30 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $( "#signupForm" ).on( "submit", function( event ) {
-        // alert( "Handler for `submit` called." );
-        // event.preventDefault();
+$(document).ready(function () {
+  console.log('ready!');
+  $('#signupForm').on('submit', function (event) {
+    // alert('Handler for `submit` called.');
+    event.preventDefault();
+    const form = document.getElementById('signupForm')
+    const formData = new FormData(form)
+    console.log(formData)
+    const payload = {};
+    formData.forEach((value, key) => (payload[key] = value));
+    // console.log(payload)
+    jsonData = JSON.stringify(payload)
+
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/users/',
+      method: 'POST',
+      data: jsonData,
+      processData: false,
+      contentType: 'application/json',
+      success: function (response) {
+        // alert('Success!');
+        console.log('Successfully signed up')
+      },
+      error: function (xhr, status, error) {
+        // alert('Your form was not sent successfully.');
+        console.log(error)
+      }
     });
+  });
 });
